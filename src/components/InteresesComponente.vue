cf77db37e3b8af74470a8a4208467e1744276cb4
Lautaro Ponzina <ponzinalautaro6@gmail.com>
2025-11-07 17:45:53 -0300

    Add files via upload

---
+++ src/components/InteresesComponente.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue';
+import FondoLava from './FondoLava.vue';
+//Este es un arreglo con ref para que se pueda reactivar el cambio de los intereses
+const intereses = ref([
+    'Programación: Me apasiona crear proyectos de software, explorar algoritmos y patrones de diseño.',
+    'Actualización Tecnológica: Mantenerme al día con los últimos lenguajes, frameworks y herramientas en el ecosistema de desarrollo.',
+]);
+</script>
+
+<template>
+    <div class="intereses-contenedor">
+        <FondoLava />
+        <ul class="contenedor-lista">
+            <li class="item" v-for="interes in intereses" :key="interes">
+                {{ interes }}
+            </li>
+        </ul>
+    </div>
+    </template>
+
+<style scoped>
+.intereses-contenedor {
+    display: flex;
+    position: relative;
+    margin: 0 auto;
+    max-width: 85%;
+    padding: 1rem;
+}
+
+.contenedor-lista {
+    list-style-type: none;
+    padding: 1rem;
+    margin-bottom: .5rem;
+    color: var(--vt-c-white-soft);
+    font-size: 1.4rem;
+    text-shadow: 3px 3px 6px rgba(0, 0, 0, 1);
+}
+
+.item {
+    margin-bottom: 1rem;
+}
+</style>
